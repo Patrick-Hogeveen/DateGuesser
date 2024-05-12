aaff4d9c52bae78f9f6d873535e3b9c44a128f06
Patrick-Hogeveen <patrick.hogeveen99@gmail.com>
2024-05-12 04:18:40 -0400

    mostly functional

--- pics.go
+++ pics.go
@@ -11,11 +11,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"os"
+	"strconv"
+	"math"
 )
 
 type Image struct {
-	QNum int
+	Id int
+	Size int
+	Type string
 	Path string
+	Year int
+	
+}
+
+type Question struct {
+	Path string
+	QuestionNum int
 }
 
 type upfile struct {
@@ -27,9 +38,17 @@ type upfile struct {
 	Count int
 }
 
+type answer struct {
+	Path string
+	QuestionNum int
+	Year int
+	Score int
+	Next int
+}
 
-var currentImages [5]int = [5]int{0,0,0,0,0}
-var validPath = regexp.MustCompile("^/(edit|save|view|static)/([a-zA-Z0-9]+)$")
+
+var currentImages [5]int = [5]int{2,3,4,0,0}
+var validPath = regexp.MustCompile("^/(edit|save|view|static|question|questionAns)/([a-zA-Z0-9]+)$")
 var templates = template.Must(template.ParseGlob("templates/*"))
 
 func dbConn() (db *sql.DB) {
@@ -83,6 +102,40 @@ func upload(w http.ResponseWriter, r *http.Request) {
 
 	db.Close()
 
+}
+
+func newGame(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	db := dbConn()
+	selDB, err := db.Query("SELECT t1.id, fsize, ftype, year, path FROM upload AS t1 JOIN (SELECT id FROM upload ORDER BY RAND() LIMIT 5) as t2 ON t1.id=t2.id")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	upld := upfile{}
+	res := []upfile{}
+	i := 0
+	for selDB.Next() {
+		var id int
+		var fname, fsize, ftype, path string
+		err = selDB.Scan(&id, &fname, &fsize, &ftype, &path)
+		if err != nil {
+			panic(err.Error())
+		}
+		upld.ID = id
+		upld.Fname = fname
+		upld.Fsize = fsize
+		upld.Ftype = ftype
+		upld.Path = path
+		res = append(res, upld)
+		currentImages[i] = upld.ID
+		i+=1
+
+	}
+	http.Redirect(w, r, "/question/1", 301)
 }
 
 func uploadFiles(w http.ResponseWriter, r *http.Request) {
@@ -148,7 +201,7 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Successfully Uploaded File\n")
 		defer db.Close()
 
-		http.Redirect(w, r, "/", 301)
+		http.Redirect(w, r, "/upload", 301)
 	}
 }
 
@@ -162,29 +215,120 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	delForm.Exec(emp)
 	log.Println("deleted successfully")
 	defer db.Close()
-	http.Redirect(w, r, "/", 301)
+	http.Redirect(w, r, "/upload", 301)
 }
 
-func imageHandler(w http.ResponseWriter, r *http.Request, path string, qNum int) {
-	q := &Image{QNum: qNum, Path: path}
-	err := templates.ExecuteTemplate(w, "image.html", q)
+func imageHandler(w http.ResponseWriter, r *http.Request, img Image, q int) {
+	question := &Question{Path:img.Path, QuestionNum: q}
+	err := templates.ExecuteTemplate(w, "image.html", question)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 }
 
-func makeImage(fn func(http.ResponseWriter, *http.Request, string, int)) http.HandlerFunc {
+func ImageById(id int) (Image, error){
+
+	db := dbConn()
+
+	var img Image
+	row := db.QueryRow("SELECT * FROM upload WHERE Id =?", id)
+	log.Printf("%v", row)
+	
+	if err := row.Scan(&img.Id, &img.Size, &img.Type, &img.Year, &img.Path); err != nil{
+		if err == sql.ErrNoRows{
+			return img, fmt.Errorf("no such image %d", id)
+		}
+		return img, fmt.Errorf("image %d: %v", id, err)
+	}
+
+	return img, nil
+}
+
+func answerHandler(w http.ResponseWriter, r *http.Request, year int, q int) {
+	db := dbConn()
+	id := currentImages[q-1]
+	var img Image
+	row := db.QueryRow("SELECT * FROM upload WHERE Id =?", id)
+	log.Printf("%v", row)
+	
+	if err := row.Scan(&img.Id, &img.Size, &img.Type, &img.Year, &img.Path); err != nil{
+		if err == sql.ErrNoRows{
+			http.NotFound(w, r)
+			log.Printf("no such image %d", id)
+			return
+			
+		}
+		http.NotFound(w, r)
+		log.Printf("image %d: %v", id, err)
+		return
+		
+	}
+
+	score := 25 - math.Abs(float64(year-img.Year))
+
+	ans := &answer{img.Path, q, img.Year, int(score), q+1}
+
+	err := templates.ExecuteTemplate(w, "answer.html", ans)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
+func makeAnswer(fn func(http.ResponseWriter, *http.Request, int, int)) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		
+		ind, err := strconv.Atoi(validPath.FindStringSubmatch(r.URL.Path)[2])
+		
+		if err != nil{
+			http.NotFound(w, r)
+			return
+		}
+		
+		year, err := strconv.Atoi(r.FormValue("year"))
+		if err != nil{
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, year,ind)
+	}
+}
+
+func makeImage(fn func(http.ResponseWriter, *http.Request, Image, int)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		db := dbConn()
-		selDB, err := db.Query("SELECT * FROM upload ORDER BY id DESC")
-		path := "http://192.168.2.245:8080/static/branch01.png"
+		
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+		ind, err := strconv.Atoi(validPath.FindStringSubmatch(r.URL.Path)[2])
+		if ind > 5 {
+			http.Redirect(w, r, "/newGame", 301)
+			return
+		}
+		
+		if err != nil{
+			http.NotFound(w, r)
+			return
+		}
+		id := currentImages[ind-1]
+		
+
+		img, err := ImageById(id)
+		log.Print( err)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
 		/*
 			if m == nil {
 				http.NotFound(w, r)
 				return
 			}*/
-		fn(w, r, path, 1)
+		fn(w, r, img, ind)
 	}
 }
 
@@ -195,7 +339,9 @@ func main() {
 	http.HandleFunc("/dele", delete)
 	http.HandleFunc("/image/", makeImage(imageHandler))
 	http.HandleFunc("/uploadfiles", uploadFiles)
-	http.HandleFunc("/question", makeImage((imageHandler)))
+	http.HandleFunc("/question/", makeImage((imageHandler)))
 	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/questionAns/", makeAnswer(answerHandler))
+	http.HandleFunc("/newGame", newGame)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
